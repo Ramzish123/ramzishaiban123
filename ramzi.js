24eceb2c29d9914e364b9bce0b7107bfac7e48f1
Ramzish123 <108720870+Ramzish123@users.noreply.github.com>
2023-04-04 01:32:47 +0300

    f
    f

---
+++ ramzi.js
@@ -0,0 +1,21 @@
+const endDate = new Date('2023-05-01T00:00:00Z');
+      
+      // Calculate the remaining days until the end date
+      function getRemainingDays() {
+        const now = new Date();
+        const timeDiff = endDate - now;
+        const daysRemaining = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+        return daysRemaining;
+      }
+      
+      // Update the countdown element with the remaining days
+      function updateCountdown() {
+        const countdownEl = document.querySelector('#countdown');
+        const daysRemaining = getRemainingDays();
+        if (daysRemaining > 0) {
+          window.location.href = 'indexx.html'; 
+        } else {
+         window.location.href = 'end.html'; 
+        }
+      }
+      setInterval(updateCountdown, 1000);
